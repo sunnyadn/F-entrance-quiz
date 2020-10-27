4765f0adb6ba1198f9ef20b2ca228d0d56c5078c
Sunny Yang <sunnyadn@foxmail.com>
2020-10-27 20:34:48 +0800

    Add group list

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import MemberList from '../components/MemberList/MemberList';
 import './App.scss';
+import GroupList from '../components/GroupList/GroupList';
 
 class App extends Component {
   render() {
     return (
       <div data-testid="app" className="App">
+        <GroupList />
         <MemberList />
       </div>
     );
---
+++ src/components/GroupList/GroupList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class GroupList extends React.PureComponent {
+  render() {
+    return (
+      <section>
+        <h1>分组列表</h1>
+      </section>
+    );
+  }
+}
